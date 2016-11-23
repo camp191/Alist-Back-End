75413249c5b984c25bb795b4019bb4b112796e50
camp191 <thanapat423@gmail.com>
2016-11-24 01:39:09 +0700

    Start Project

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var postCSS = require('gulp-postcss');
+var autoPrefixer = require('autoprefixer');
+var varCSS = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var importCSS = require('postcss-import');
+
+gulp.task('css', function(){
+  return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postCSS([importCSS, varCSS, autoPrefixer, nested]))
+        .on('error', function(errorInfo){
+          console.log(errorInfo.toString());
+          this.emit('end');
+        })
+        .pipe(gulp.dest('./app/temp/styles'));
+})
